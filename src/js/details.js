eded2cb4525513167d1cba8f9b717747424e39c9
maskhb <734387564@qq.com>
2017-05-11 20:45:18 +0800

    bugxiugai

--- src/js/details.js
+++ src/js/details.js
@@ -17,19 +17,30 @@ $(function() {
     var $currentImg = $('#detail_content>.left>.zoom img')
     var cloneImg = $currentImg[0].clone
     $('.addToCar').on('click', '.btnAdd', function(e) {
-    	//克隆并且移动商品
+            //克隆并且移动商品
             $currentImg.clone(true).appendTo('body').css({
-                    'position': 'absolute',
+                'position': 'absolute',
+                "left": 30,
+                "top": 242,
+                'width': 80
+            })
+
+            .fly({
+                start: {
                     "left": 30,
-                    "top": 242,
-                    'width':350
-                }).animate({"left":839,"top":14,"width":80},function(){
-                	$(this).animate({"left":1240,"top":186,"width":20},function(){
-                		$(this).remove();
-                		alert('已添加购物车');
-                	});
-                });
+                    "top": 242
+                },
+                end: {
+                    "left": 1240,
+                    "top": 186,
+                    "width": 20
+                },
+                onEnd: function() {
+                    alert('已添加购物车');
+                    
+                    this.destory();
+                }
+            });
         })
         //目标值 x:30---839---1240 y 242---14--186
-
 })
